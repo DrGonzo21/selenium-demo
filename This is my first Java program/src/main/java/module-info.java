4405950e7c298737b3ec4598b686383c4a3290cd
DrGonzo21 <134796946+DrGonzo21@users.noreply.github.com>
2023-06-05 17:04:20 -0700

    Delete This is my first Java program directory

--- This is my first Java program/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.thisismyfirstjavaprogram {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.thisismyfirstjavaprogram to javafx.fxml;
-    exports com.example.thisismyfirstjavaprogram;
-}
